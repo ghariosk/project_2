92c9f21a9cbf42c53ebab6f842cd72ed41829afb
LukeH1993 <lwheudebourck@gmail.com>
2017-10-09 21:37:43 +0100

    Added two controllers and removed irrelevant html erb files

--- projects_sparta/config/routes.rb
+++ projects_sparta/config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'projects_controller/index'
+
+  get 'projects_controller/show'
+
+  get 'projects_controller/new'
+
+  get 'projects_controller/create'
+
+  get 'projects_controller/edit'
+
+  get 'projects_controller/update'
+
+  get 'projects_controller/destroy'
+
+  get 'users_controller/index'
+
+  get 'users_controller/show'
+
+  get 'users_controller/new'
+
+  get 'users_controller/create'
+
+  get 'users_controller/edit'
+
+  get 'users_controller/update'
+
+  get 'users_controller/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
