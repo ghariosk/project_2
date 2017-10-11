218fb2a499ebd26ac0c9dfddfefdd3d0bbdf2fcd
ghariosk <karlgharios@hotmail.com>
2017-10-11 23:31:05 +0100

    added comments to the projects controller

--- projects_sparta/app/controllers/projects_controller.rb
+++ projects_sparta/app/controllers/projects_controller.rb
@@ -1,22 +1,22 @@
 class ProjectsController < ApplicationController
   def index
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) # sets the index page of the projects to yield a list of all the projects assigned to the user's id
     @projects=@user.projects
   end
 
   def show
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) # sets the show page of the projects to display the project associated to the user and id
     @project= @user.projects.find(params[:id])
   end
 
   def new
-    @user=@user=User.all.find(params[:user_id])
+    @user=@user=User.all.find(params[:user_id]) # create a new database entry for a project and of USerProject, linking the user to the project
     @project=Project.new
     @userproject=UserProject.new
   end
 
   def create
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) # hydrate the data into the newly created databse of project and userproject
     new_project=Project.create(project_params)
     @userproject=UserProject.create(project_id: new_project.id, user_id:@user.id)
     @userproject.save
@@ -24,19 +24,19 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) # extract the data from the user and project database to edit
     @project=@user.projects.find(params[:id])
   end
 
 
   def update
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) #  update the database with the new entries
     update=@user.projects.find(params[:id]).update(project_params)  
     redirect_to user_projects_path
   end
 
   def destroy
-    @user=User.all.find(params[:user_id])
+    @user=User.all.find(params[:user_id]) # deletes the database entry associated with the :id
     Project.destroy(params[:id])
     redirect_to user_projects_path
   end
@@ -44,7 +44,8 @@ class ProjectsController < ApplicationController
   protected
 
   def project_params
-    params.require(:project).permit(:id, :name, :desc, :image, :git, :approved)
+    params.require(:project).permit(:id, :name, :desc, :image, :git, :approved) # permits the exchanges of data between the controller 
+                                                                                # and the database
   end
 
   def user_params
