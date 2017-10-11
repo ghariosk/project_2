25feba43ea8070f7357b9a50f560bc64e6855558
ghariosk <karlgharios@hotmail.com>
2017-10-11 23:33:17 +0100

    added comments to the registrations controller

--- projects_sparta/app/controllers/public_controller.rb
+++ projects_sparta/app/controllers/public_controller.rb
@@ -1,11 +1,11 @@
 class PublicController < ApplicationController
 	def index
-		redirect_to root_url
+		redirect_to root_url # routes the public/index url to home
 	end
 	def show
-		@project=Project.find(params[:id])
+		@project=Project.find(params[:id]) # routes the show page to display a single project of id set by the page parameter
 	end
 	def home 
-		@projects=Project.all
+		@projects=Project.all # routes the home page to display a list of all project
 	end
 end
