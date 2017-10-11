e6ce0cf1ee3aa9107621f70972904856ca014fd8
ghariosk <karlgharios@hotmail.com>
2017-10-11 23:36:31 +0100

    added comments to the users controller

--- projects_sparta/app/controllers/users_controller.rb
+++ projects_sparta/app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  if user_signed_in?
+  before_action :authenticate_user! # limits the accessibility to signed in users only
+  if user_signed_in? 
     def index
-      if current_user.admin == true
-        @users=User.all
+      if current_user.admin == true # show a list of all users registered if the current user is an admin
+        @users=User.all 
         @users.each do |element| 
          @task=element
         end
       else 
-        @user=current_user
+        @user=current_user. # or else sho a list of the user's personal page . not very relevant here as current_user required
         @userproject=UserProject.where(user_id:current_user.id)
         redirect_to root_url
       end
     end
-    def show
+    def show # will display the personal page of one user determined by the :id
     	@user=User.find(params[:id])
     end
-    def edit
+    def edit # permits the update of the extra paramets of the user
       if 
         @user = User.find(params[:id])
       end
     end
-    def update
+    def update 
       user = User.find(params[:id])
       user.update(user_params)
       edirect_to users_index_url
