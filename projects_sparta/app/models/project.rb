c3fa12860200d37c799741750ce5cee2651e2464
Priyam Patel <priyampatel1994@gmail.com>
2017-10-15 11:02:44 +0100

    qwqw

--- projects_sparta/app/models/project.rb
+++ projects_sparta/app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
 	through: :user_project
   
 	validates :name, :desc,:image, :git, presence: true
-	validates :name, length: { minimum: 3, maximum: 100, message: "must be between 3 and 100 characters!" }
+	validates :name, length: { minimum: 3, maximum: 100, message: "who thefuck has a name thats that short/long, are you fuck bro? Is your momma fucked bro cos she named you that shit" }
 
 	mount_uploader :image, ImageUploader
 
