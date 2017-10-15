0a9fb97296970efee391b59e3f8abe5be7c2f2b4
Priyam Patel <priyampatel1994@gmail.com>
2017-10-15 12:20:00 +0100

    as

--- projects_sparta/app/models/project.rb
+++ projects_sparta/app/models/project.rb
@@ -13,7 +13,7 @@ class Project < ApplicationRecord
 	has_many :users, 
 	through: :user_project
   
-	validates :name, :desc,:image, :git, presence: true
+	validates :name, :desc, :git, presence: true
 	validates :name, length: { minimum: 3, maximum: 100, message: "who the.fuck has a name thats that short/long, are you fuck bro? Is your momma fucked bro cos she named you that shit" }
 
 end
